3cb19bd8c16d249269b2f60561da29bd0816cc6e
Chaos Chen <czy426@icloud.com>
2022-04-08 20:33:41 +0800

    set rainbow parentheses max line limit

--- lua/modules/treesitter.lua
+++ lua/modules/treesitter.lua
@@ -42,7 +42,7 @@ configs.setup({
 		enable = true,
 		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		max_file_lines = 3000, -- Do not enable for files with more than n lines, int
 		colors = {
 			"#CCFFFF",
 			"#0066CC",
