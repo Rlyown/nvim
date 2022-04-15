d7d41d408d23f07578ee5fce5ec76469f3248da2
Chaos Chen <czy426@icloud.com>
2022-04-16 02:17:51 +0800

    pyright setup

--- lua/modules/lsp/settings/pyright.lua
+++ lua/modules/lsp/settings/pyright.lua
@@ -5,7 +5,9 @@ return {
 				autoSearchPaths = true,
 				diagnosticMode = "workspace",
 				useLibraryCodeForTypes = true,
+				autoImportCompletions = true,
 			},
 		},
+		-- pythonPath = "/usr/bin/python3",
 	},
 }
