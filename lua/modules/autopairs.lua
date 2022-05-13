5a6ed0a5db516156c9c89cd5c20f501a5e10f66d
Chaos Chen <czy426@icloud.com>
2022-05-13 20:57:28 +0800

    fix multi brackets bug after auto complete

--- lua/modules/autopairs.lua
+++ lua/modules/autopairs.lua
@@ -28,11 +28,6 @@ npairs.setup({
 		highlight = "PmenuSel",
 		highlight_grey = "LineNr",
 	},
+	disable_in_macro = false,
+	enable_check_bracket_line = true,
 })
-
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-	return
-end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
