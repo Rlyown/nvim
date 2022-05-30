af1a290803c5d57ada43366ad6524bc204d293d3
Chaos Chen <czy426@icloud.com>
2022-05-31 00:52:13 +0800

    update: nvim-tree setting migration

--- lua/modules/nvim-tree.lua
+++ lua/modules/nvim-tree.lua
@@ -1,26 +1,3 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "S",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-	folder = {
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-	},
-}
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
 	return
@@ -100,6 +77,28 @@ nvim_tree.setup({
 		},
 		icons = {
 			webdev_colors = true,
+			glyphs = {
+				-- following options are the default
+				-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
 		},
 	},
 	trash = {
