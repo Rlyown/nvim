783f7ef452f796c2113f47b02dbe38318ef492a5
Chaos Chen <czy426@icloud.com>
2022-12-05 18:24:17 +0800

    fix: update dressing config

--- lua/modules/dressing.lua
+++ lua/modules/dressing.lua
@@ -31,11 +31,6 @@ return function()
 			max_width = { 140, 0.9 },
 			min_width = { 20, 0.2 },
 
-			-- Window transparency (0-100)
-			winblend = 10,
-			-- Change default highlight groups (see :help winhl)
-			winhighlight = "",
-
 			override = function(conf)
 				-- This is the config that will be passed to nvim_open_win.
 				-- Change values here to customize the layout
@@ -100,11 +95,6 @@ return function()
 				border = "rounded",
 				-- 'editor' and 'win' will default to being centered
 				relative = "editor",
-
-				-- Window transparency (0-100)
-				winblend = 10,
-				-- Change default highlight groups (see :help winhl)
-				winhighlight = "",
 
 				-- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
 				-- the min_ and max_ options can be a list of mixed types.
