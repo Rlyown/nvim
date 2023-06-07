5a6a0d7181dc272ada0c8eaab14b3b6eed4f2aa9
Chaos Chen <czy426@icloud.com>
2023-06-07 23:47:06 +0800

    feat: set wrap for `tex` file

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -2,62 +2,62 @@ local configs = require("modules.configs")
 
 -- Related issue: https://github.com/L3MON4D3/LuaSnip/issues/258
 function _G.leave_snippet()
-	if
-		((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-		and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-		and not require("luasnip").session.jump_active
-	then
-		require("luasnip").unlink_current()
-	end
+    if
+        ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+        and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require("luasnip").session.jump_active
+    then
+        require("luasnip").unlink_current()
+    end
 end
 
 -- example: t = {{bname=bufname, filetype=filetype}}
 function _G.check_last_special_win(t)
-	local name_cases = {
-		["NvimTree_" .. vim.fn.tabpagenr()] = true,
-		["OUTLINE"] = true,
-		["mind"] = true,
-		-- ["DAP Scopes"] = true,
-		-- ["DAP Breakpoints"] = true,
-		-- ["DAP Stacks"] = true,
-		-- ["DAP Watches"] = true,
-		-- ["[dap-repl]"] = true,
-		-- ["[dap-terminal] Launch"] = true,
-	}
+    local name_cases = {
+        ["NvimTree_" .. vim.fn.tabpagenr()] = true,
+        ["OUTLINE"] = true,
+        ["mind"] = true,
+        -- ["DAP Scopes"] = true,
+        -- ["DAP Breakpoints"] = true,
+        -- ["DAP Stacks"] = true,
+        -- ["DAP Watches"] = true,
+        -- ["[dap-repl]"] = true,
+        -- ["[dap-terminal] Launch"] = true,
+    }
 
-	local type_cases = {
-		["Outline"] = true,
-		-- ["dapui_breakpoints"] = true,
-		-- ["dapui_scopes"] = true,
-		-- ["dapui_stacks"] = true,
-		-- ["dapui_watches"] = true,
-		-- ["dap-rel"] = true,
-		-- ["dapui_console"] = true,
-	}
+    local type_cases = {
+        ["Outline"] = true,
+        -- ["dapui_breakpoints"] = true,
+        -- ["dapui_scopes"] = true,
+        -- ["dapui_stacks"] = true,
+        -- ["dapui_watches"] = true,
+        -- ["dap-rel"] = true,
+        -- ["dapui_console"] = true,
+    }
 
-	if not t or #t == 0 then
-		return false
-	end
+    if not t or #t == 0 then
+        return false
+    end
 
-	for _, value in ipairs(t) do
-		if value.bname and name_cases[value.bname] then
-		elseif value.filetype and type_cases[value.btype] then
-		else
-			return false
-		end
-	end
+    for _, value in ipairs(t) do
+        if value.bname and name_cases[value.bname] then
+        elseif value.filetype and type_cases[value.btype] then
+        else
+            return false
+        end
+    end
 
-	return true
+    return true
 end
 
 function _G.set_terminal_keymaps()
-	local opts = { noremap = true }
-	-- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true }
+    -- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 local augroup = vim.api.nvim_create_augroup
@@ -65,99 +65,114 @@ local autocmd = vim.api.nvim_create_autocmd
 
 augroup("_CUSTOM_general_settings", { clear = true })
 autocmd("FileType", {
-	group = "_CUSTOM_general_settings",
-	pattern = { "qf", "help", "man", "lspinfo", "startuptime", "null-ls-info", "notify", "spectre_panel" },
-	command = "nnoremap <silent> <buffer> q :close<CR>",
+    group = "_CUSTOM_general_settings",
+    pattern = { "qf", "help", "man", "lspinfo", "startuptime", "null-ls-info", "notify", "spectre_panel" },
+    command = "nnoremap <silent> <buffer> q :close<CR>",
 })
 autocmd("TextYankPost", {
-	group = "_CUSTOM_general_settings",
-	pattern = "*",
-	command = "silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})",
+    group = "_CUSTOM_general_settings",
+    pattern = "*",
+    command = "silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})",
 })
 autocmd("BufWinEnter", {
-	group = "_CUSTOM_general_settings",
-	pattern = "*",
-	command = ":set formatoptions-=cro",
+    group = "_CUSTOM_general_settings",
+    pattern = "*",
+    command = ":set formatoptions-=cro",
 })
 autocmd("FileType", {
-	group = "_CUSTOM_general_settings",
-	pattern = "qf",
-	command = "set nobuflisted",
+    group = "_CUSTOM_general_settings",
+    pattern = "qf",
+    command = "set nobuflisted",
 })
 
 augroup("_CUSTOM_git", { clear = true })
 autocmd("FileType", {
-	group = "_CUSTOM_git",
-	pattern = "gitcommit",
-	command = "setlocal wrap",
+    group = "_CUSTOM_git",
+    pattern = "gitcommit",
+    command = "setlocal wrap",
 })
 autocmd("FileType", {
-	group = "_CUSTOM_git",
-	pattern = "gitcommit",
-	command = "setlocal spell",
+    group = "_CUSTOM_git",
+    pattern = "gitcommit",
+    command = "setlocal spell",
 })
 
-augroup("_CUSTOM_markdown", { clear = true })
+augroup("_CUSTOM_text", { clear = true })
 autocmd("FileType", {
-	group = "_CUSTOM_markdown",
-	pattern = "markdown",
-	command = "setlocal wrap",
+    group = "_CUSTOM_text",
+    pattern = "norg",
+    callback = function()
+        vim.api.nvim_buf_set_option(0, "shiftwidth", 2)
+        vim.api.nvim_buf_set_option(0, "tabstop", 2)
+    end,
 })
 autocmd("FileType", {
-	group = "_CUSTOM_markdown",
-	pattern = "markdown",
-	command = "setlocal spell",
+    group = "_CUSTOM_text",
+    pattern = { "norg", "markdown" },
+    callback = function()
+        vim.opt_local.conceallevel = 3
+    end,
+})
+autocmd("FileType", {
+    group = "_CUSTOM_text",
+    pattern = { "norg", "markdown", "tex" },
+    command = "setlocal wrap",
+})
+autocmd("FileType", {
+    group = "_CUSTOM_text",
+    pattern = { "norg", "markdown", "tex" },
+    command = "setlocal spell",
 })
 
 augroup("_CUSTOM_auto_resize", { clear = true })
 autocmd("VimResized", {
-	group = "_CUSTOM_auto_resize",
-	pattern = "*",
-	callback = function()
-		require("bufresize").resize()
-	end,
+    group = "_CUSTOM_auto_resize",
+    pattern = "*",
+    callback = function()
+        require("bufresize").resize()
+    end,
 })
 
 augroup("_CUSTOM_alpha", { clear = true })
 autocmd("User", {
-	group = "_CUSTOM_alpha",
-	pattern = "AlphaReady",
-	command = "set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2",
+    group = "_CUSTOM_alpha",
+    pattern = "AlphaReady",
+    command = "set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2",
 })
 
 augroup("_CUSTOM_lsp", { clear = true })
 autocmd("BufWritePre", {
-	group = "_CUSTOM_lsp",
-	pattern = "*",
-	callback = function()
-		local filter = {
-			norg = true,
-		}
+    group = "_CUSTOM_lsp",
+    pattern = "*",
+    callback = function()
+        local filter = {
+            norg = true,
+        }
 
-		if filter[vim.bo.filetype] then
-			return
-		end
+        if filter[vim.bo.filetype] then
+            return
+        end
 
-		if vim.g.custom_enable_auto_format then
-			if vim.bo.filetype == "go" then
-				require("go.format").goimport()
-			else
-				vim.lsp.buf.format({ async = false })
-			end
-		end
-	end,
-	desc = "auto format",
+        if vim.g.custom_enable_auto_format then
+            if vim.bo.filetype == "go" then
+                require("go.format").goimport()
+            else
+                vim.lsp.buf.format({ async = false })
+            end
+        end
+    end,
+    desc = "auto format",
 })
 -- Issue: https://github.com/nvim-telescope/telescope.nvim/issues/559
 autocmd("BufRead", {
-	group = "_CUSTOM_lsp",
-	pattern = "*",
-	callback = function()
-		vim.api.nvim_create_autocmd("BufWinEnter", {
-			once = true,
-			command = "normal! zx",
-		})
-	end,
+    group = "_CUSTOM_lsp",
+    pattern = "*",
+    callback = function()
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            once = true,
+            command = "normal! zx",
+        })
+    end,
 })
 -- Question: https://superuser.com/questions/567352/how-can-i-set-foldlevelstart-in-vim-to-just-fold-nothing-initially-still-allowi
 -- autocmd("BufWinEnter", {
@@ -168,114 +183,94 @@ autocmd("BufRead", {
 
 augroup("_CUSTOM_auto_close", { clear = true })
 autocmd("BufEnter", {
-	group = "_CUSTOM_auto_close",
-	pattern = "*",
-	callback = function()
-		local wins = {}
-		local winnr = vim.fn.winnr("$")
+    group = "_CUSTOM_auto_close",
+    pattern = "*",
+    callback = function()
+        local wins = {}
+        local winnr = vim.fn.winnr("$")
 
-		for w = 1, winnr do
-			local buf = vim.fn.winbufnr(w)
-			wins[#wins + 1] = {
-				bname = vim.fn.bufname(buf),
-				btype = vim.fn.getbufvar(buf, "&filetype"),
-			}
-		end
+        for w = 1, winnr do
+            local buf = vim.fn.winbufnr(w)
+            wins[#wins + 1] = {
+                bname = vim.fn.bufname(buf),
+                btype = vim.fn.getbufvar(buf, "&filetype"),
+            }
+        end
 
-		if check_last_special_win(wins) then
-			vim.cmd("quit")
-		end
-	end,
-	nested = true,
+        if check_last_special_win(wins) then
+            vim.cmd("quit")
+        end
+    end,
+    nested = true,
 })
 
 -- stop snippets when you leave to normal mode
 augroup("_CUSTOM_luasnip", { clear = true })
 autocmd("ModeChanged", {
-	group = "_CUSTOM_luasnip",
-	pattern = "*",
-	callback = leave_snippet,
+    group = "_CUSTOM_luasnip",
+    pattern = "*",
+    callback = leave_snippet,
 })
 
 augroup("_CUSTOM_cmp", { clear = true })
 autocmd("FileType", {
-	group = "_CUSTOM_cmp",
-	pattern = "toml",
-	callback = function()
-		require("cmp").setup.buffer({ sources = { { name = "crates" } } })
-	end,
+    group = "_CUSTOM_cmp",
+    pattern = "toml",
+    callback = function()
+        require("cmp").setup.buffer({ sources = { { name = "crates" } } })
+    end,
 })
 
 augroup("_CUSTOM_terminal", { clear = true })
 autocmd("TermOpen", {
-	group = "_CUSTOM_terminal",
-	pattern = { "term://*" },
-	callback = function()
-		set_terminal_keymaps()
-		vim.wo.spell = false
-	end,
+    group = "_CUSTOM_terminal",
+    pattern = { "term://*" },
+    callback = function()
+        set_terminal_keymaps()
+        vim.wo.spell = false
+    end,
 })
 autocmd("FileType", {
-	group = "_CUSTOM_terminal",
-	pattern = { "dapui_console" },
-	callback = function()
-		set_terminal_keymaps()
-		vim.wo.spell = false
-	end,
+    group = "_CUSTOM_terminal",
+    pattern = { "dapui_console" },
+    callback = function()
+        set_terminal_keymaps()
+        vim.wo.spell = false
+    end,
 })
 
 augroup("_CUSTOM_compile", { clear = true })
 -- Create an autocmd User PackerCompileDone to update it every time packer is compiled
 autocmd("User", {
-	group = "_CUSTOM_compile",
-	pattern = "LazyDone",
-	callback = function()
-		require("catppuccin").compile() -- Catppuccin also provide a function to work with the catppuccin compiler.
+    group = "_CUSTOM_compile",
+    pattern = "LazyDone",
+    callback = function()
+        require("catppuccin").compile() -- Catppuccin also provide a function to work with the catppuccin compiler.
 
-		vim.defer_fn(function()
-			vim.cmd("colorscheme catppuccin")
-		end, 50) -- Debounced for live reloading
-	end,
+        vim.defer_fn(function()
+            vim.cmd("colorscheme catppuccin")
+        end, 50) -- Debounced for live reloading
+    end,
 })
 vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = { "catppuccin.lua" },
-	callback = function()
-		require("catppuccin").compile() -- Catppuccin also provide a function to work with the catppuccin compiler.
-	end,
-})
-
-augroup("_CUSTOM_neorg", { clear = true })
-autocmd("FileType", {
-	group = "_CUSTOM_neorg",
-	pattern = "norg",
-	callback = function()
-		vim.api.nvim_buf_set_option(0, "shiftwidth", 2)
-		vim.api.nvim_buf_set_option(0, "tabstop", 2)
-	end,
-})
-
--- Only enable conceal for these filetypes.
-augroup("_CUSTOM_conceal", { clear = true })
-autocmd("FileType", {
-	group = "_CUSTOM_conceal",
-	pattern = { "norg", "markdown" },
-	callback = function()
-		vim.opt_local.conceallevel = 3
-	end,
+    pattern = { "catppuccin.lua" },
+    callback = function()
+        require("catppuccin").compile() -- Catppuccin also provide a function to work with the catppuccin compiler.
+    end,
 })
 
 augroup("_CUSTOM_VimIM", { clear = true })
 autocmd("User", {
-	group = "_CUSTOM_VimIM",
-	pattern = "ZFVimIM_event_OnEnable",
-	callback = function()
-		require("noice").cmd("disable")
-	end,
+    group = "_CUSTOM_VimIM",
+    pattern = "ZFVimIM_event_OnEnable",
+    callback = function()
+        require("noice").cmd("disable")
+    end,
 })
 autocmd("User", {
-	group = "_CUSTOM_VimIM",
-	pattern = "ZFVimIM_event_OnDisable",
-	callback = function()
-		require("noice").cmd("enable")
-	end,
+    group = "_CUSTOM_VimIM",
+    pattern = "ZFVimIM_event_OnDisable",
+    callback = function()
+        require("noice").cmd("enable")
+    end,
 })
