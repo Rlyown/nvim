f447d8c6012817de0d4eb9c6a82c4fc34d6fa7db
Chaos Chen <czy426@icloud.com>
2024-01-15 16:09:15 +0800

    update: set color for Normal and Comment

--- lua/modules/catppuccin.lua
+++ lua/modules/catppuccin.lua
@@ -5,6 +5,37 @@ return function()
     catppuccin.setup({
         transparent_background = false,
         term_colors = true,
+        highlight_overrides = {
+            -- all = function(colors)
+            --     return {
+            --         NvimTreeNormal = { fg = colors.none },
+            --         CmpBorder = { fg = "#3e4145" },
+            --     }
+            -- end,
+            -- latte = function(latte)
+            --     return {
+            --         Normal = { fg = latte.base },
+            --     }
+            -- end,
+            -- frappe = function(frappe)
+            --     return {
+            --         ["@comment"] = { fg = frappe.surface2, style = { "italic" } },
+            --     }
+            -- end,
+            -- macchiato = function(macchiato)
+            --     return {
+            --         LineNr = { fg = macchiato.overlay1 },
+            --     }
+            -- end,
+            mocha = function(mocha)
+                return {
+                    Comment = { fg = "#b3e0ff" },
+                    -- Comment = { fg = "#cccccc" },
+                    -- Comment = { fg = "#ffffcc" }
+                    Normal = { fg = "#ffffff" }
+                }
+            end,
+        },
         integrations = {
             lsp_trouble = true,
             nvimtree = {
